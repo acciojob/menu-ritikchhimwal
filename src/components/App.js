c0f617fc9cae5e9e7d2a525e8782815609ec3c41
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-12-26 19:56:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+import './style.css';
 
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
